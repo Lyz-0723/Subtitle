986b2a50d9a90c52f245f2248e42569de58bffec
laudantstolam <rachelle0607@gmail.com>
2023-06-16 15:27:07 +0800

    bug fixed and add circle function

--- component/Timer.py
+++ component/Timer.py
@@ -17,6 +17,7 @@ class Timer:
         self.timer_text = Text('字体管家方萌.TTF', '25:00', 120, (710, 140), self.color)
         self.start_count = False
         self.end = False
+        self.state = False
 
     def animation(self):
         self.current_time -= 1
@@ -27,14 +28,26 @@ class Timer:
         self.current_time = time
 
     def show(self, screen: pygame.Surface):
-        m, s = self.current_time // 60, self.current_time % 60
+        if self.current_time >=0 :
+            m, s = self.current_time // 60, self.current_time % 60
+        else:
+            m,s=0,0
+            self.color = DEFAULT_COLOR_GRAY
+            self.start_count = False #數到0的時候暫停數數，顯示灰色
+            self.end = True
+
         self.timer_text.update(f"{m:02}:{s:02}", self.color)
         self.timer_text.show(screen)
         # screen.blit(self.timer_text, self.timer_text_rect)
 
     def timer_pressed(self, pos):  # 被按下的瞬間的設定
+        print("state = ",self.start_count)
         if self.timer_text.rect.collidepoint(pos):
             self.state = True
+            if self.end:
+                self.current_time = WORKING_LENGTH
+                self.end = False
+
         else:
             self.state = False
 
@@ -43,5 +56,7 @@ class Timer:
 
     def _count_compressed(self):
         if self.state:
+            if self.current_time == 0 :
+                self.current_time = 1500
             self.start_count = not self.start_count
             self.color = DEFAULT_COLOR if self.color == DEFAULT_COLOR_GRAY else DEFAULT_COLOR_GRAY
